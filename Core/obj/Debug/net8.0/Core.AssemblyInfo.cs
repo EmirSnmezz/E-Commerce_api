d1fa63e5eab688359f299b1140dba23c66d93dd5
emir <emircan_snmez@outlook.com>
2024-11-03 02:47:16 +0300

    .

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f32d9d16fa8b7d1efac14becc8e80cb1cc5a3cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe9cc9d3224ed4049d194561b673777c2918c5e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
