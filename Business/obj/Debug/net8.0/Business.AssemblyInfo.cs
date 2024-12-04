3619aad0b9131481e57aea08d88466e84b1be2e4
emir <79750775+EmirSnmezz@users.noreply.github.com>
2024-12-05 01:15:05 +0300

    Category entity added in business layer

--- Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
+++ Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4e06bf4c71b4a2a73cbe39fe8fb40921ea0ef88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+407121829c80a7ec2d2d11ca1b0d6cd8266dcd1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
