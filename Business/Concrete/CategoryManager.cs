3c234a0999022bdfb41b3eace92038549ece7917
emir <79750775+EmirSnmezz@users.noreply.github.com>
2024-12-05 01:51:04 +0300

    ICategoryDal implemented in CategoryManager

--- Business/Concrete/CategoryManager.cs
+++ Business/Concrete/CategoryManager.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,21 @@ namespace Business.Concrete
 {
     public class CategoryManager : ICategoryService
     {
+        public ICategoryDal _categoryDal;
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            //iş kodları
+            return _categoryDal.GetAll();
+        }
+
+        public Category GetById(int id)
+        {
+            return _categoryDal.Get( category => Convert.ToInt32(category.CategoryId) == id);
+        }
     }
 }
