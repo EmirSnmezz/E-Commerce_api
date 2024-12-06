0eae301c9b8742d8e9abae849162d6c221f29f8a
emir <79750775+EmirSnmezz@users.noreply.github.com>
2024-12-06 03:32:26 +0300

    Join operation created for product detail and productId's type of id type changed to string

--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,10 +1,27 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entity.Concrete;
+using Entity.DTOs;
 
 namespace DataAccess.Concrete.EntityFramework
 {
     public class EfProductDal : EntityRepositoryBase<Product, NorthwindDbContext>, IProductDal
     {
+        public List<ProductDetailDTO> GetProductDetails(int id)
+        {
+            using (var db = new NorthwindDbContext()) 
+            {
+                ProductDetailDTO result = (from p in db.Products
+                                          join c in db.Categories
+                                          on p.CategoryId equals c.CategoryId
+                                          select new ProductDetailDTO
+                                          {
+                                              CategoryName = c.CategoryName,
+                                              ProductId = (int)Convert.ToUInt32(p.ProductId),
+                                              ProductName = p.ProductName,
+                                              UnitsInStock = p.UnitsInStock
+                                          }).FirstOrDefault();
+            }
+        }
     }
 }
