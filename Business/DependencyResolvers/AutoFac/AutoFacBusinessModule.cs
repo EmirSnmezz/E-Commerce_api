277b9e67eca3084a9f2ad4f5a189c0272f632262
emir <79750775+EmirSnmezz@users.noreply.github.com>
2024-12-16 07:36:21 +0300

    AutoFac IoC container Added on Project for AOP. Created AutoFacBusinessModule, apply configuring AutoFac IoC Settings (change default IoC Provider To AutoFac Factory Provider.

---
+++ Business/DependencyResolvers/AutoFac/AutoFacBusinessModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+namespace Business.DependencyResolvers.AutoFac
+{
+    public class AutoFacBusinessModule : Module
+    {
+        // program.cs'deki dependecy resolver işlemi Autofac'de burada gerçekleşmektedir.
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance(); // biri IProductService isterse ona ProductManager ver demenin AutoFac'deki karşılığı budur.
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+        }
+    }
+}
--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d5ce6df99047f665074190cef54272398a21660")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca80a8be867e5a0dcbda5da1fefc213a0976db76")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
