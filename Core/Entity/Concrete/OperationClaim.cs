516d4db71b1cbfedf15d6b6c105f52c31bee86e8
Emir <emir.sonmez@id3.com.tr>
2025-01-29 03:17:16 +0300

    entites created for jwt implemention

---
+++ Core/Entity/Concrete/OperationClaim.cs
@@ -0,0 +1,10 @@
+﻿using Entity.Abstract;
+
+namespace Core.Entity.Concrete
+{
+    public class OperationClaim : IEntity
+    {
+        int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
